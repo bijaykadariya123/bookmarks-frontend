f00f1c6e71cadee005f8f619a49cadf34e407155
Bijay Kadariya <bijaykadariya92@gmail.com>
2023-08-13 16:51:59 -0400

    imported loader dependencies

--- src/router.js
+++ src/router.js
@@ -6,14 +6,14 @@ import {
 import App from "./App";
 import Index from "./pages/Index";
 import Show from "./pages/Show";
-import { bookmarksLoader, bookmarkLoader } from "./loaders";
+import {  bookmarkLoader, onebookmarkLoader } from "./loaders";
 import { createAction, updateAction, deleteAction } from "./actions";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="" element={<Index />} loader={bookmarkLoader} />
-      <Route path=":id" element={<Show />} loader={oneBookmarkLoader}/>
+      <Route path=":id" element={<Show />} loader={onebookmarkLoader}/>
       {/* <Route path=":id" element={<Show />} loader={bookmarkLoader} /> */}
       <Route path="create" action={createAction} />
       <Route path="update/:id" action={updateAction} />
