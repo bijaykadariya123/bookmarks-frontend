8bd7913d1c896a5d95b1cf73a57e1e2063ae8a8a
Bijay Kadariya <bijaykadariya92@gmail.com>
2023-08-13 19:26:45 -0400

    googleFonts

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -4,22 +4,26 @@ function Index(props) {
   const bookmarks = useLoaderData();
 
   return (
-    <div>
-      <h2>Create a bookmark</h2>
-      <Form action="/create" method="post">
-        <input type="input" name="title" placeholder="bookmark's title" />
-        <input type="input" name="url" placeholder="bookmark's url" />
-        <input type="submit" value="create bookmark" />
-      </Form>
+    <div className="indexcss">
+          <div className="index-main">
+          <h2>Create a bookmark</h2>
+          <Form action="/create" method="post">
+            <label>Input Title</label><br/>
+            <input type="input" name="title" placeholder="bookmark's title" /><br/>
+            <label>Input URL</label><br/>
+            <input type="input" name="url" placeholder="bookmark's url" /><br/>
+            <input type="submit" value="create bookmark" />
+          </Form>
 
-      <h2>Bookmarks</h2>
-      {bookmarks.map((bookmark) => {
-        return <div key={bookmark._id} className="bookmark">
-          <Link to={`/${bookmark._id}`}>
-            <h3>{bookmark.title}</h3>
-          </Link>
+          <h2>Bookmarks</h2>
+          {bookmarks.map((bookmark) => {
+            return <div key={bookmark._id} className="bookmark">
+              <Link to={`/${bookmark._id}`}>
+                <h4>{bookmark.title}</h4>
+              </Link>
+            </div>
+        })}
         </div>
-    })}
     </div>
 )}
 
